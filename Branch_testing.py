62acae7f7ec4da9fb21b8e2ea0aa861c647c5c00
Cynthia S <thiahuff@gmail.com>
2020-10-27 02:42:53 +0000

    added function to greet bestfriend

--- Branch_testing.py
+++ Branch_testing.py
@@ -1,2 +1,5 @@
 def say_hello_to_friends(friends_name):
     return f"Hello {friends_name}, I hope you have a lovely day!"
+
+def say_hello_to_bff(best_friend):
+    return f"Hello {best_friend}, I am so excited to see you!"
