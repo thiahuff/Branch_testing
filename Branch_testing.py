e407d072da2df0089f42522f7e13c2d02713229e
Cynthia S <thiahuff@gmail.com>
2020-10-27 03:27:23 +0000

    updated say_hello_to_bff to compliment Kaitlin's dress

--- Branch_testing.py
+++ Branch_testing.py
@@ -2,5 +2,5 @@ def say_hello_to_friends(friends_name):
     return f"Hello {friends_name}, I hope you have a lovely day!"
 
 
-def say_hello_to_bff(kaitlin_shirt_color):
-    return f"Hello Kaitlin, I love your {kaitlin_shirt_color} shirt, you look amazing!"
+def say_hello_to_bff(kaitlin_dress_color):
+    return f"Hello Kaitlin, I love your {kaitlin_dress_color} dress, you look amazing!"
